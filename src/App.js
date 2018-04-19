986974e3dff0d5adcf1a8066682d8938e4c281e1
Lev Ekasov <l.ekasov@hh.ru>
2018-04-19 16:41:58 +0300

    add indicator modal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
 import {Button, Col, ControlLabel, FormControl, Grid, Modal, Row, Table} from 'react-bootstrap';
+import ButtonToolbar from "react-bootstrap/es/ButtonToolbar";
 
 const Months = Object.freeze({
-                                 Jan: 0,
-                                 Feb: 1,
-                                 Mar: 2,
-                                 Apr: 3,
-                                 May: 4,
-                                 Jun: 5,
-                                 Jul: 6,
-                                 Aug: 7,
-                                 Sep: 8,
-                                 Oct: 9,
-                                 Nov: 10,
-                                 Dec: 11
+                                 Jan: 1,
+                                 Feb: 2,
+                                 Mar: 3,
+                                 Apr: 4,
+                                 May: 5,
+                                 Jun: 6,
+                                 Jul: 7,
+                                 Aug: 8,
+                                 Sep: 9,
+                                 Oct: 10,
+                                 Nov: 11,
+                                 Dec: 12
                              });
 
 const dateTimeFormatter = Intl.DateTimeFormat('en-GB', {
@@ -27,7 +28,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentMonth: Months.Apr,
+            currentMonth: Months.May,
             indicators: [
                 {
                     name: 'Текущая страница книги Джедайские техники',
@@ -51,33 +52,92 @@ class App extends Component {
 }
 
 class IndicatorsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showCreateIndicator: false,
+            indicators: props.indicators,
+        };
+
+        this.handleShowCreateIndicator = this.handleShowCreateIndicator.bind(this);
+        this.handleCloseCreateIndicator = this.handleCloseCreateIndicator.bind(this);
+        this.handleAddIndicator = this.handleAddIndicator.bind(this);
+    }
+
+    handleCloseCreateIndicator() {
+        this.setState({ showCreateIndicator: false });
+    }
+
+    handleShowCreateIndicator() {
+        this.setState({ showCreateIndicator: true });
+    }
+
+    handleAddIndicator() {
+        let indicators = this.state.indicators;
+        indicators.push({name: this.state.newIndicatorName, history: []});
+
+        this.setState({
+            indicators: indicators,
+                      });
+
+        this.handleCloseCreateIndicator();
+    }
+
     render() {
-        const indicators = this.props.indicators.map(
+        const indicators = this.state.indicators.map(
             indicator => <Indicator history={indicator.history}
                                     key={indicator.name}
                                     name={indicator.name}
+                                    month={this.props.currentMonth}
             />);
         return (
             <div>
                 <h1>Indicators</h1>
+                <ButtonToolbar>
+                    <Button onClick={this.handleShowCreateIndicator}>Добавить индикатор</Button>
+                </ButtonToolbar>
                 <Table striped bordered condensed hover>
                     <MonthHeader month={this.props.currentMonth} name='Indicators'/>
                     <tbody>
                     {indicators}
                     </tbody>
                 </Table>
+                <Modal show={this.state.showCreateIndicator} onHide={this.handleCloseCreateIndicator}>
+                    <Modal.Header>
+                        Создание нового индикатора
+                    </Modal.Header>
+                    <Modal.Body>
+                        <ControlLabel>Введите название индикатора</ControlLabel>
+                        <FormControl
+                            autoFocus
+                            type="text"
+                            value={this.state.value}
+                            placeholder="Enter text"
+                            onChange={(e) => this.setState({newIndicatorName: e.target.value})}
+                            onKeyPress={(event) => {
+                                if(event.key === 'Enter'){this.handleAddIndicator();}}}
+                        />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.handleAddIndicator} bsStyle="primary">Сохранить</Button>
+                        <Button onClick={this.handleCloseCreateIndicator}>Отмена</Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
     }
 }
 
 function MonthHeader(props) {
-    const daysInMonth = new Date(2018, props.month, 0).getDate();
-
+    let date = new Date(2018, props.month, 0);
+    const daysInMonth = date.getDate();
+    // console.log(date + ' ' + date.getDate());
+    // console.log('days in month ' + props.month + ' are ' + daysInMonth);
     var dates = [];
-    for (var i = 1; i < daysInMonth; i++) {
+    for (var i = 1; i <= daysInMonth; i++) {
+        // console.log(i);
         dates.push(<th key={i} className="rotate">
-            <div>{dateTimeFormatter.format(new Date(2018, props.month, i))}</div>
+            <div>{dateTimeFormatter.format(new Date(2018, props.month-1, i))}</div>
         </th>);
     }
     return (
@@ -106,11 +166,22 @@ class Indicator extends Component {
     }
 
     render() {
-        let ths = this.state.history.map((value, day) => <IndicatorValue day={day}
-                                                                         key={day}
-                                                                         indicatorName={this.props.name}
-                                                                         currentValue={value}
-                                                                         onValueChange={(newValue, day) => this.handleValueChange(newValue, day)}/>);
+        const daysInMonth = new Date(2018, this.props.month, 0).getDate();
+        console.log(this.props.name + ' ' + daysInMonth + ' ' + this.props.month);
+        let ths = Array(daysInMonth);
+        for (var index = 0; index < daysInMonth; index++) {
+            let value = index >= this.state.history.length? 0: this.state.history[index];
+            ths.push(<IndicatorValue day={index}
+                                     key={index}
+                                     indicatorName={this.props.name}
+                                     currentValue={value}
+                                     onValueChange={(newValue, day) => this.handleValueChange(newValue, day)}/>);
+        }
+        // let ths = this.state.history.map((value, day) => <IndicatorValue day={day}
+        //                                                                  key={day}
+        //                                                                  indicatorName={this.props.name}
+        //                                                                  currentValue={value}
+        //                                                                  onValueChange={(newValue, day) => this.handleValueChange(newValue, day)}/>);
         return (
             <tr>
                 <td>
@@ -157,7 +228,7 @@ class IndicatorValue extends Component {
 
     render() {
         return (<td>
-            <p onClick={this.handleShow}>{this.props.currentValue === 0 ? "-": this.props.currentValue}</p>
+            <p onClick={this.handleShow}>{(this.props.currentValue === 0 ||  !this.props.currentValue)? "-": this.props.currentValue}</p>
             <Modal show={this.state.show} onHide={this.handleClose}>
                 <Modal.Header>
                     {this.props.indicatorName}
